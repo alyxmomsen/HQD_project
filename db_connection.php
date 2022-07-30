750bc59c368b87abfc47fc1853081022afcf3958
alex <shizofranky@gmail.com>
2022-07-30 14:09:06 +0300

    test commit

--- db_connection.php
+++ db_connection.php
@@ -3,4 +3,9 @@
 $pdo = new PDO("mysql:host=localhost;dbname=another;cahrset=utf8", 'root', '');
 // $pdo = new PDO("mysql:host=localhost;dbname=ci18760_testsite;cahrset=utf8", 'ci18760_testsite', 'Henaros42');
 // comment
+
+
+
+
+// this is another comment
 ?>
