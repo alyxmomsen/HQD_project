50a973a7f830b9f58d3fc6d551a7ee2ac88227ba
dobolli <my.own.gin@yandex.ru>
2022-07-30 18:12:12 +0300

    clear comments

--- db_connection.php
+++ db_connection.php
@@ -2,11 +2,5 @@
 
 $pdo = new PDO("mysql:host=localhost;dbname=another;cahrset=utf8", 'root', '');
 // $pdo = new PDO("mysql:host=localhost;dbname=ci18760_testsite;cahrset=utf8", 'ci18760_testsite', 'Henaros42');
-// comment
-$var = 0 ;
 
-
-// comment else
-
-// this is another comment
 ?>
