cd4a283d91ba40faae3264a3cd09a3caaf2adc83
alex <shizofranky@gmail.com>
2022-08-01 17:16:24 +0300

    remove db_connection from indexed files

--- db_connection.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$pdo = new PDO("mysql:host=localhost;dbname=another;cahrset=utf8", 'root', '');
-// $pdo = new PDO("mysql:host=localhost;dbname=ci18760_testsite;cahrset=utf8", 'ci18760_testsite', 'Henaros42');
-
-?>
